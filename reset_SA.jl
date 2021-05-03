4765eb0f7876bc9e31fed239f3521507b4516a58
Aranguren-Code <47396259+Aranguren-Code@users.noreply.github.com>
2021-05-03 17:09:55 -0500

    Delete reset_SA.jl

--- reset_SA.jl
+++
@@ -1,9 +0,0 @@
-using JuMP
-
-for i in 1:D
-    JuMP.fix(D_Model[i],0)
-end
-print(" \r\n")
-print("Successfull! \r\n")
-print("All Depots have been set to zero (variable D_Model[j]=0)")
-print(" \r\n")
